36dcb75fafe00f6a74cd1b51e1267cf4783ed25d
Skye Flowers <snflow2005@gmail.com>
2025-09-10 17:11:09 -0500

    cleaning up files

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-//document.getElementById("count-el").innerText = 5
-
-// let lapsCompleted = 0
-
-// function increaseLaps() {
-//     lapsCompleted = lapsCompleted + 1
-// }
-// increaseLaps()
-// console.log(lapsCompleted)
-
-// increaseLaps()
-// console.log(lapsCompleted)
-
-// increaseLaps()
-// console.log(lapsCompleted)
-
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked (log it out)
-// change the count-el in the HTML to reflect the new count
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
 let count = 0
@@ -33,7 +13,3 @@ function save() {
     count = 0
     countEl.textContent = count
 }
-let message = "Hi, my name is "
-let username = "skyef"
-let myGreeting = message + username
-console.log(myGreeting)
